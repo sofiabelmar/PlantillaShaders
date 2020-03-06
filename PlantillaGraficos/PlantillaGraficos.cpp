ea39c85cb8bab5565c9e2de79e11dbec85afa843
sofiabelmar <42181326+sofiabelmar@users.noreply.github.com>
2020-03-06 10:48:28 -0700

    comunicacion con el shader

--- PlantillaGraficos/PlantillaGraficos.cpp
+++ PlantillaGraficos/PlantillaGraficos.cpp
@@ -55,7 +55,7 @@ void dibujar() {
 
 int main()
 {
-    //Declarar una ventana
+	//Declarar una ventana
 	GLFWwindow* window;
 
 	//Si no se pudo iniciar GLFW
@@ -93,8 +93,29 @@ int main()
 	InicializarTriangulo();
 
 	const char* rutaVertexShader = "VertexShader.shader";
-		const char* rutaFragmentShader = "FragmentShader.shader";
-	shader = new Shader(rutaVertexShader,rutaFragmentShader);
+	const char* rutaFragmentShader = "FragmentShader.shader";
+	shader = new Shader(rutaVertexShader, rutaFragmentShader);
+
+	//Mapeo de atributos
+	posicionID = glGetAttribLocation(shader->getID(), "posicion");
+	colorID = glGetAttribLocation(shader->getID(), "color");
+
+	shader->desenlazar();
+
+	//Crear el vertex array del triangulo
+	glGenVertexArrays(1, &vertexArrayTrianguloID);
+	glBindVertexArray(vertexArrayTrianguloID);
+
+	//Vertex buffer
+	glGenBuffers(1, &bufferTrianguloID);
+	glBindBuffer(GL_ARRAY_BUFFER, bufferTrianguloID);
+	//Asociar datos al buffer
+	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertice) * triangulo.size(), triangulo.data(),GL_STATIC_DRAW);
+	//Habilitar atrbutos de shader
+	glEnableVertexAttribArray(posicionID);
+	glEnableVertexAttribArray(colorID);
+	//Especificar a OpenGL como comunicarse
+	glVertexAttribPointer(posicionID, 3, GL_FLOAT, GL_FALSE, sizeof(Vertice), 0);
 
 	//Ciclo de dibujo (Draw loop)
 	while (!glfwWindowShouldClose(window)) {
